c40a1f84e62a2a9e6cd1ae1da5989c4cd74fbd4b
Justin Lung <justin.lung@hva.nl>
2022-12-29 12:01:54 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import { Hero } from '../components/Hero/Hero'
-import InfoCardContainer from "../components/OurDishes/OurDishes"
-
 function Home() {
   return (
     <>
-      <Hero />
-      <InfoCardContainer />
+
     </>
   )
 }
